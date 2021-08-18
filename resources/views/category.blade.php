704fd412e5afa6d0417f63f2456f8f554048b209
kevzhangz <kevzhangz888@gmail.com>
2021-08-18 13:52:11 +0700

    Factory & Faker

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -4,8 +4,9 @@
     <h1>Post Category : {{ $category }}</h1>
 
     @foreach($posts as $post)
-        <article>
+        <article class="mb-4 border-bottom">
             <h2><a class="text-decoration-none" href="/blog/{{ $post->slug }}">{{ $post->title }}</a></h2>
+            <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a class="text-decoration-none" href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
             <p>{{ $post->excerpt }}</p>
         </article>
     @endforeach
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,7 +4,7 @@
     <article>
         <h2>{{ $post->title }}</h2>
     
-        <p>By. <a href="/about" class="text-decoration-none pb-2">{{ $post->user->name }}</a> in <a class="text-decoration-none" href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+        <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none pb-2">{{ $post->author->name }}</a> in <a class="text-decoration-none" href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
 
         {!! $post->body !!}
     </article>
